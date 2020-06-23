82be86e85f0a02e1c3bf334fe9a8e1ed8b74ea59
Admin <Admin@192.168.76.10>
2020-06-23 17:01:13 +0900

    db

--- miniproject/don_sql/don.sql
+++ miniproject/don_sql/don.sql
@@ -14,3 +14,25 @@ increment by 1
 nocache;
 
 select * from members;
+
+drop table members purge;
+drop table calendar purge;
+
+create table cashbook(
+regdate   date not null,
+money    number default 0 not null);
+
+create table members(
+no number primary key,
+id varchar2(20) not null,
+password varchar2(128) not null, 
+name varchar2(20) not null,
+e_mail varchar2(30) not null);
+
+create sequence members_no_seq
+start with 1
+increment by 1
+nocache;
+
+select * from members;
+select * from calendar;
